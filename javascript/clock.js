18d434190fa47bd60c2b98492f4ccbbe580c53ba
Pablo Santalla <pablosantalla92@gmail.com>
2024-12-06 13:53:28 -0800

    update

--- javascript/clock.js
+++ javascript/clock.js
@@ -4,43 +4,28 @@ function updateClock() {
   var timeString = date.toLocaleTimeString('en-US', options);
   var clockElements = document.getElementsByClassName("com-clock--la");
 
-  // Remove space between hour and 'a' or 'p'
   var modifiedTimeString = timeString.replace(" AM", "a").replace(" PM", "p");
 
-  // Get abbreviated day name in Los Angeles timezone
   var dayName = date.toLocaleDateString('en-US', { weekday: 'short', timeZone: 'America/Los_Angeles' });
 
-  // Create a date for the Los Angeles timezone to get the correct hour
   var laDate = new Date().toLocaleString('en-US', { timeZone: 'America/Los_Angeles' });
   var laDateObj = new Date(laDate);
   var hour = laDateObj.getHours();
   var icon = "";
 
-  if (hour >= 22 || hour < 7) {
-    icon = "🛌"; // Sleeping icon
-  } else if (hour >= 7 && hour < 8) {
-    icon = "☕️"; // Coffee icon
-  } else if (hour >= 8 && hour < 9) {
-    icon = "🚗"; // Driving to work icon
-  } else if (hour >= 9 && hour < 17) {
-    icon = "💼"; // Working icon
-  } else if (hour >= 17 && hour < 18) {
-    icon = "🚗"; // Driving home icon
-  } else if (hour >= 18 && hour < 19) {
-    icon = "🎾"; // Tennis icon
-  } else if (hour >= 19 && hour < 20) {
-    icon = "🍳"; // Cooking icon
-  } else if (hour >= 20 && hour < 21) {
-    icon = "📺"; // Watching TV icon
-  } else if (hour >= 21 && hour < 22) {
-    icon = "🎮"; // Playing video games icon
-  } else if (hour >= 23 || hour < 0) { // 11 PM to 12 AM (midnight)
-    icon = "🛌"; // Sleeping icon
-  } else if ((dayName === "Sat" || dayName === "Sun") && hour >= 9 && hour < 11) {
-    icon = "🚶"; // Weekend walk icon
-  } else if (hour >= 13 && hour < 15) {
-    icon = "📚"; // Reading icon
-  }
+  // Icon based on LA time
+  if (hour >= 22 || hour < 7) icon = "🛌";
+  else if (hour >= 7 && hour < 8) icon = "☕️";
+  else if (hour >= 8 && hour < 9) icon = "🚗";
+  else if (hour >= 9 && hour < 17) icon = "💼";
+  else if (hour >= 17 && hour < 18) icon = "🚗";
+  else if (hour >= 18 && hour < 19) icon = "🎾";
+  else if (hour >= 19 && hour < 20) icon = "🍳";
+  else if (hour >= 20 && hour < 21) icon = "📺";
+  else if (hour >= 21 && hour < 22) icon = "🎮";
+  else if (hour >= 23 || hour < 0) icon = "🛌";
+  else if ((dayName === "Sat" || dayName === "Sun") && hour >= 9 && hour < 11) icon = "🚶";
+  else if (hour >= 13 && hour < 15) icon = "📚";
 
   var message = "It's " + dayName + " " + modifiedTimeString + " here, so I'm probably " + icon;
 
@@ -51,4 +36,5 @@ function updateClock() {
   }
 }
 
+// Update every second
 setInterval(updateClock, 1000);
