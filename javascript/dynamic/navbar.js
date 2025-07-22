a4f6557e62bf882c360e89770db6bbc287857b1f
Pablo Santalla <pablosantalla92@gmail.com>
2025-07-21 22:25:46 -0700

    js included in origin

--- javascript/dynamic/navbar.js
+++ javascript/dynamic/navbar.js
@@ -9,6 +9,18 @@ fetch(basePath + 'html/dynamic/navbar.html')
 })
 .then(function(navbarHTML) {
   navbarContainer.innerHTML = navbarHTML;
+  
+  document.getElementById('logo-link').href = basePath + 'index.html';
+  document.querySelector('a[href="about.html"]').href = basePath + 'about.html';
+  document.querySelector('a[href="blog.html"]').href = basePath + 'blog.html';
+  
+  var logoImg = document.querySelector('.com-logo');
+  var emailImg = document.querySelector('img[alt="Icon of an envelope."]');
+  var burgerImg = document.querySelector('.com-burger');
+  
+  if (logoImg) logoImg.src = basePath + 'assets/logo.svg';
+  if (emailImg) emailImg.src = basePath + 'assets/email.svg';
+  if (burgerImg) burgerImg.src = basePath + 'assets/burger-menu-dotted.svg';
 })
 .catch(function(error) {
   console.log('Failed to load navbar HTML:', error);
