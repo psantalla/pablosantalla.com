76f4fcc21185a3166dee43564fec13e41317bc52
Pablo Santalla <pablosantalla92@gmail.com>
2025-07-21 22:29:24 -0700

    footer paths

--- javascript/dynamic/footer.js
+++ javascript/dynamic/footer.js
@@ -9,6 +9,16 @@ fetch(basePath + 'html/dynamic/footer.html')
 })
 .then(function(footerHTML) {
   footerContainer.innerHTML = footerHTML;
+  
+  if (isInSubfolder) {
+    var footerLinks = document.querySelectorAll('#footer-js a[href]');
+    footerLinks.forEach(function(link) {
+      var href = link.getAttribute('href');
+      if (href && !href.startsWith('http') && !href.startsWith('mailto:') && !href.startsWith('../')) {
+        link.href = '../' + href;
+      }
+    });
+  }
 })
 .catch(function(error) {
   console.log('Failed to load footer HTML:', error);
